511acde0069b88b065d0fc88d8cb834fa74924c4
Joao Comba <joao.comba@gmail.com>
2018-10-10 15:50:08 -0300

    changed color table

--- src/app/demo2/demo2.component.ts
+++ src/app/demo2/demo2.component.ts
@@ -183,7 +183,7 @@ export class Demo2Component implements OnInit, AfterViewInit {
 
   color: any;
 
-  color_range = ['#a50026', '#d73027', '#f46d43', '#fdae61', '#fee090', '#ffffbf', '#e0f3f8', '#abd9e9', '#74add1', '#4575b4', '#313695'].reverse();
+  color_range = ['#a50026','#d73027','#f46d43','#fdae61','#fee08b','#ffffbf','#d9ef8b','#a6d96a','#66bd63','#1a9850','#006837'].reverse();
 
   prev_dim = '';
 
